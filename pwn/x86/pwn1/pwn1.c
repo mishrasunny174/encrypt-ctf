c5a10e543bd144e685425270b0d9900f7196a966
mishrasunny174 <mishrasunny174@gmail.com>
2019-02-26 17:29:22 +0530

    modified pwn1 challenge

--- pwn/x86/pwn1/pwn1.c
+++ pwn/x86/pwn1/pwn1.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<unistd.h>
 
+void shell(){
+	system("/bin/bash");
+}
+
 int main(){
 	char buffer[128];
 	setvbuf(stdout, NULL, _IONBF, 0);
 	printf("Tell me your name: ");
 	gets(buffer);
 	printf("Hello, %s\n",buffer);
+	//shell();
 	return 0;
 }
