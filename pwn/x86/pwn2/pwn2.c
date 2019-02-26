3505c72af4e4891fef80fed9047216d3fb59631f
mishrasunny174 <mishrasunny174@gmail.com>
2019-02-26 20:51:25 +0530

    added pwn2

--- pwn/x86/pwn2/pwn2.c
+++ pwn/x86/pwn2/pwn2.c
@@ -1,10 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 void run_command_ls() {
 	system("ls");
 }
 
+void lol() {
+	__asm__("jmp %esp");
+}
+
 int main(){
 	char buffer[32];
-	
+	setvbuf(stdout, NULL,_IONBF,0);
+	printf("$ ");
+	gets(buffer);
+	if(strcmp(buffer,"ls")==0) {
+		run_command_ls();
+	} else {
+		printf("bash: command not found: %s\n",buffer);
+	}
+	printf("Bye!\n");
+	return 0;
 }
