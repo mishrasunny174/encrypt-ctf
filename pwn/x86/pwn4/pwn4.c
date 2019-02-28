43889e91e40c033505aacbb89d3eb1a135d62868
mishrasunny174 <mishrasunny174@gmail.com>
2019-02-28 18:26:00 +0530

    added pwn4 and coombined all docker-compose

---
+++ pwn/x86/pwn4/pwn4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void __() {
+	system("/bin/bash");
+}
+
+int main(){
+	char buffer[128];
+	puts("Do you swear to use this shell with responsility by the old gods and the new?\n");
+	gets(buffer);
+	printf(buffer);
+	printf("\ni don't belive you!\n%s\n",buffer);
+	return 0;
+}
