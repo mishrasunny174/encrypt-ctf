3c5ea080189ad859f4a34605f60e9d7c9f9f58a6
eshaan7 <ishubansal2000@gmail.com>
2019-04-02 18:06:58 +0530

    added solve.py

--- web/75_env/app/application.py
+++ web/75_env/app/application.py
@@ -8,18 +8,19 @@ app = Flask(__name__)
 '''
  secret_key using python3 secrets module
 '''
-app.secret_key = "9d367b3ba8e8654c6433379763e80c6e"
 
-'''
-Learn about virtualenv here:
-https://www.youtube.com/watch?v=N5vscPTWKOk&list=PL-osiE80TeTt66h8cVpmbayBKlMTuS55y&index=7
-'''
+app.secret_key = "9d367b3ba8e8654c6433379763e80c6e"
 
 FLAG = "encryptCTF{v1rtualenvs_4re_c00l}"
 
 @app.route('/')
+@app.route('/home')
 def index():
 	return render_template('index.html')
+
+@app.route('/whatsthetime')
+def whatsthetime():
+	
 
 @app.route('/whatsthetime/<int:what_the_timestamp>', methods=["GET", "POST"])
 def getflag(what_the_timestamp):
